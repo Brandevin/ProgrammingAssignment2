6d35ca7f354f8140c84c521d1d5957588067f3d8
Brandevin <felipe.courasuzuki@gmail.com>
2020-11-10 21:53:46 -0300

    1st version to be evaluated. Based on the functions provided by the author to cache a mean value

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix returns a list of 4 functions that are used to help
+## caching the data
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m<-NULL
+      set <-function(y){
+        x<<- y
+        m<<- NULL
+      }
+      get<-function() x
+      setinverse <- function(inverse) m<<-inverse
+      getinverse <- function() m
+      list(set=set,get=get,
+           setinverse=setinverse,
+           getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Cache solve function returns the inverse of the matrix x
+## If it was already calculated, returns the cached value
+## If it is the first time called, calculates it by using solve()
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m<- x$getinverse()
+        if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+        }
+        data <- x$get()
+        print(data)
+        m <- solve(data)
+        x$setinverse(m)
+        m
 }
+
+M<-matrix(rnorm(9),nrow=3,ncol=3)
+
+matrix<-makeCacheMatrix(M)
+cacheSolve(matrix)
